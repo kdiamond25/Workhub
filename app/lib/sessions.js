e625c69e7d3d3ce99319776795dee5715eb7650c
kdiamond25 <kim.diamond@truecitrus.com>
2026-04-28 09:34:22 -0400

    Add sessions.js lib

--- app/lib/sessions.js
+++ app/lib/sessions.js
@@ -13,5 +13,5 @@ export function getSession(id) {
 }
 
 export function generateId() {
-  return Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
+  return Math.random().toString(36).slice(2) + Date.now().toString(36) + Math.random().toString(36).slice(2)
 }
